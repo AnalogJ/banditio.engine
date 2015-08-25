ce8cb21d9f63ac6dbe6c5e9f117ce188d43e2550
Jason Kulatunga <jk17@ualberta.ca>
2015-08-24 22:10:45 -0700

    update to clean up unneeded agents. Calling it an engine (will be run inside the proxy container)

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM mitmproxy/mitmproxy
 
-WORKDIR /srv/banditio.inspector.backend
+WORKDIR /srv/banditio.engine
 
 #install the pip libraries
 RUN pip install tornado
@@ -8,7 +8,7 @@ RUN pip install git+https://github.com/dgrtwo/ParsePy.git
 
 
 #clone the application code.
-RUN git clone https://github.com/AnalogJ/banditio.inspector.backend.git .
+RUN git clone https://github.com/AnalogJ/banditio.engine.git .
 
 # Default command
-CMD ["python","server.py"]
+CMD ["python","engine.py"]
--- agent/page.py
+++ agent/page.py
@@ -6,7 +6,7 @@ class Page():
             https://developer.chrome.com/devtools/docs/protocol/1.1/page#page.enable
         """
         return {
-            "result": {}
+            "error": "Unimplemented"
         }
 
     @staticmethod
@@ -15,6 +15,6 @@ class Page():
             "error": "Unimplemented"
         }
 
-    @staticmethod
-    def getResourceTree():
-        return {"result": {"frameTree": {"frame": {"url": "http://www.chromium.org/", "mimeType": "text/html", "loaderId": "7897.2", "id": "7897.1", "securityOrigin": "http://www.chromium.org"}, "resources": [{"url": "http://www.chromium.org/_/rsrc/1439160688000/system/app/css/camelot/allthemes-view.css", "mimeType": "text/css", "type": "Stylesheet"}, {"url": "http://www.gstatic.com/sites/p/ae80a1/system/app/themes/beigeandblue/standard-css-beigeandblue-ltr-ltr.css", "mimeType": "text/css", "type": "Stylesheet"}, {"url": "http://www.chromium.org/_/rsrc/1438811752264/chromium-projects/logo_chrome_color_1x_web_32dp.png", "mimeType": "image/png", "type": "Image"}, {"url": "http://www.chromium.org/_/rsrc/1439160688000/system/app/css/overlay.css?cb=beigeandblueundefineda100%25%25150goog-ws-leftthemedefaultstandard", "mimeType": "text/css", "type": "Stylesheet"}, {"url": "http://www.gstatic.com/sites/p/ae80a1/system/js/jot_min_view__en.js", "mimeType": "text/javascript", "type": "Script"}, {"url": "http://www.chromium.org/_/rsrc/1438879449147/config/customLogo.gif?revision=3", "mimeType": "image/png", "type": "Image"}]}}}
+    #@staticmethod
+    #def getResourceTree():
+    #    return {"result": {"frameTree": {"frame": {"url": "http://www.chromium.org/", "mimeType": "text/html", "loaderId": "7897.2", "id": "7897.1", "securityOrigin": "http://www.chromium.org"}, "resources": [{"url": "http://www.chromium.org/_/rsrc/1439160688000/system/app/css/camelot/allthemes-view.css", "mimeType": "text/css", "type": "Stylesheet"}, {"url": "http://www.gstatic.com/sites/p/ae80a1/system/app/themes/beigeandblue/standard-css-beigeandblue-ltr-ltr.css", "mimeType": "text/css", "type": "Stylesheet"}, {"url": "http://www.chromium.org/_/rsrc/1438811752264/chromium-projects/logo_chrome_color_1x_web_32dp.png", "mimeType": "image/png", "type": "Image"}, {"url": "http://www.chromium.org/_/rsrc/1439160688000/system/app/css/overlay.css?cb=beigeandblueundefineda100%25%25150goog-ws-leftthemedefaultstandard", "mimeType": "text/css", "type": "Stylesheet"}, {"url": "http://www.gstatic.com/sites/p/ae80a1/system/js/jot_min_view__en.js", "mimeType": "text/javascript", "type": "Script"}, {"url": "http://www.chromium.org/_/rsrc/1438879449147/config/customLogo.gif?revision=3", "mimeType": "image/png", "type": "Image"}]}}}
